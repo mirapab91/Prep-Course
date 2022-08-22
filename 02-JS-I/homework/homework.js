4237feadc965566a34a0b54a7e50f30e9d4ec7e9
mirapab91 <pani_miranda@hotmail.com>
2022-08-22 13:11:35 -0300

    finish ultimo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -64,7 +64,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x == y){
+  if (x === y){
     return true;
   }
   return false;
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 == str2){
+  if (str1 === str2){
     return true;    
   }
   else {
@@ -87,8 +87,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num - 90){
+  if (num < 90){
     return true;
+  }
+  else {
+    return false;
   }
   
 }
@@ -100,12 +103,16 @@ function mayorQueCincuenta(num) {
   if (num > 50){
     return true;
   }
+  else {
+    return false;
+  }
   
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  
   
 }
 
